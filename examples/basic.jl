247418574785c05bab1d99bd3560cb9f840d7171
Meredith Julian <mjulian@mit.edu>
2021-03-15 17:56:29 -0400

    basic example

---
+++ examples/basic.jl
@@ -0,0 +1,19 @@
+using JuLoop
+
+instructions = [Instruction(:mult, :(out[i, j] += A[i, k] * B[k, j]), Set()),
+                Instruction(:double, :(out[i, j] *= 2), Set([:mult]))]
+
+domains = [Domain(:i, 1, :(size(out, 1)), :(i += 1), Set(), []),
+           Domain(:j, 1, :(size(out, 2)), :(j += 1), Set(), []),
+           Domain(:k, 1, :(size(A, 2)), :(k += 1), Set(), [])]
+
+
+kern = compile_native_julia(LoopKernel(instructions, domains, [:out, :A, :B], [Array{Float64, 2}, Array{Float64, 2}, Array{Float64, 2}]))
+
+A = rand(10, 10)
+B = rand(10, 10)
+out = zeros(10, 10)
+
+kern(out=out, A=A, B=B)
+
+@show isapprox(out, A*B*2)
